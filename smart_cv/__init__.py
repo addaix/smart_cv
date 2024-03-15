86abf132714890b1f5518c6a5e85822ffd894714
Thor Whalen <thorwhalen1@gmail.com>
2024-03-15 14:13:16 +0100

    coding...

---
+++ smart_cv/__init__.py
@@ -0,0 +1,7 @@
+"""
+The smart_cv package provides functionality for creating and managing smart CVs (resumes).
+
+This package contains modules for parsing and analyzing CV data, generating 
+CV templates, and extracting information from CV documents.
+"""
+
