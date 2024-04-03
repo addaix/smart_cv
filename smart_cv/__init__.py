30ec78fe06d2049ca42bd549e0e6eec3b6d750c5
davidaleprog <alexisdavid1402@gmail.com>
2024-04-03 10:15:24 +0200

    deleted:    ChunkDB.py 	modified:   README.md 	modified:   misc/ad_resume_parsing.ipynb 	modified:   smart_cv/__init__.py 	renamed:    smart_cv/data/cvs/CV_Adil_Mektoub.pdf -> smart_cv/data/cvs/Adil.pdf 	modified:   smart_cv/util.py

--- smart_cv/__init__.py
+++ smart_cv/__init__.py
@@ -6,6 +6,4 @@ CV templates, and extracting information from CV documents.
 """
 
 from smart_cv.base import CvsInfoStore, CvsFilesReader
-from smart_cv.ResumeParser import ContentRetriever
-from smart_cv.ResumeParser import TemplateFiller
-from smart_cv.CriteriaChecker import CriteriaChecker
+from smart_cv.ResumeParser import ContentRetriever, TemplateFiller
--- smart_cv/util.py
+++ smart_cv/util.py
@@ -4,6 +4,7 @@ from importlib.resources import files
 import json
 import tiktoken
 from PyPDF2 import PdfReader
+from langchain_community.document_loaders.word_document import Docx2txtLoader
 
 pkg_name = "smart_cv"
 
@@ -23,8 +24,6 @@ def read_config(config_file: str) -> dict:
     assert "template" in d, "Please provide a template path in the config file."
     assert "prompts" in d, "Please provide prompts in the config file."
     assert "api_key" in d, "Please provide an API key in the config file."
-    
-
     return d
 
 def num_tokens_from_string(string: str, encoding_name: str="cl100k_base") -> int:
@@ -32,21 +31,19 @@ def num_tokens_from_string(string: str, encoding_name: str="cl100k_base") -> int
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
-
-def num_tokens_doc(doc: str, encoding_name: str = "cl100k_base") -> int:
-    num_tokens = 0
-    with open(doc, "rb") as f:  # Open in binary mode
-        pdf = PdfReader(f)
-        for page in pdf.pages:  # Iterate over pages directly
-            text = page.extract_text()
-            num_tokens += num_tokens_from_string(text, encoding_name)
-    return num_tokens
-
-
+from docx import Document
 def load_full_text(doc: str):
     text = ""
-    with open(doc, "rb") as f:  # Open in binary mode
-        pdf = PdfReader(f)
-        for page in pdf.pages:  # Iterate over pages directly
-            text += page.extract_text()
-    return text        
+    if doc.endswith(".docx"):
+        doc = Document(doc)
+        for para in doc.paragraphs:
+            text += para.text
+    elif doc.endswith(".pdf"):
+        with open(doc, "rb") as f:  # Open in binary mode
+            pdf = PdfReader(f)
+            for page in pdf.pages:  # Iterate over pages directly
+                text += page.extract_text()
+    return text     
+
+def num_tokens_doc(doc: str, encoding_name: str = "cl100k_base") -> int:
+    return num_tokens_from_string(load_full_text(doc), encoding_name)
